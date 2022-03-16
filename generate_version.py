09a44331cdd8716ff803f4bedf7cb381ea175509
NooBat <khoi27012003@gmail.com>
2022-03-16 10:00:40 +0700

    commit by vscode

--- generate_version.py
+++ generate_version.py
@@ -1,13 +1,15 @@
 import os
 
-def generate_version(path_to_dir, new_version=True):
+def generate_version(path_to_dir, new_file_name='', new_version=True):
     """Create a new folder contains new_version weights file
 
-    Returns new_version, path to saving folder and path to save weights file (preferably .hdf5 files)
+    Returns new_version, path to saving folder and path to save new_file_name (preferably .hdf5 files)
 
     Keyword arguments:
 
     path_to_dir -- path to directory where new folder should be created
+
+    new_file_name -- name of the desired file (default to '')
 
     new_version -- version of the saved model and new weights file (default to 1)
     """
@@ -22,10 +24,11 @@ def generate_version(path_to_dir, new_version=True):
         new_version = versions_list[-1] if len(versions_list) > 0 else 1
 
     new_version_folder = 'version{}'.format(int(new_version))
-    new_version_file = 'weights.best.hdf5'
+    new_version_file = new_file_name if len(new_file_name) > 0 else None
 
-    path = os.path.join(path_to_dir, new_version_folder)
-    save_path = os.path.join(path, new_version_file)
+    save_path = path = os.path.join(path_to_dir, new_version_folder)
+    if new_version_file is not None:
+        save_path = os.path.join(save_path, new_version_file)
     if versions_list.count(new_version) == 0:
         os.makedirs(path)
 
